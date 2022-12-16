650b99b33752b6821ffaa3fb7a5effb94dee27f5
hyunji1203 <hj27720@gmail.com>
2022-12-16 22:46:44 +0900

    feat: 다리길이 입력받기

--- src/main/kotlin/bridge/newRefactoring/UI/InputV.kt
+++ src/main/kotlin/bridge/newRefactoring/UI/InputV.kt
@@ -1,7 +1,11 @@
-package bridge.newRefactoring.UI
+package bridge.newRefactoring.UI4
+
+import camp.nextstep.edu.missionutils.Console
 
 class InputV {
     fun readBridgeSize(): Int {
+        println("다리의 길이를 입력해주세요.")
+        var size = Console.readLine()
         return 0
     }
 
