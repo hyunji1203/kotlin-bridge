fab2152742a87d6e722d037d7dc0e60684296a65
hyunji1203 <hj27720@gmail.com>
2022-12-16 23:18:18 +0900

    feat: 다리 이동

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -2,6 +2,7 @@ package bridge
 
 import bridge.newRefactoring.UI.OutputV
 import bridge.newRefactoring.UI4.InputV
+import bridge.newRefactoring.domain.BridgeGameNew
 import bridge.newRefactoring.domain.BridgeMakerNew
 
 fun main() {
@@ -10,6 +11,17 @@ fun main() {
     //다리 생성
     var bridgeNumberGenerator = BridgeRandomNumberGenerator()
     var bridge = BridgeMakerNew(bridgeNumberGenerator).makeBridge(InputV().readBridgeSize())
+    var userBridge = mutableListOf<String>()
+
+    //다리 이동
+    while (bridge != userBridge){
+        var moving = InputV().readMoving()
+        userBridge.add(moving)
+        var bridgeMatch = BridgeGameNew().move(userBridge, bridge)
+
+
+    }
+
 }
 
 
--- src/main/kotlin/bridge/newRefactoring/domain/BridgeGameNew.kt
+++ src/main/kotlin/bridge/newRefactoring/domain/BridgeGameNew.kt
@@ -1,7 +1,14 @@
 package bridge.newRefactoring.domain
 
 class BridgeGameNew {
-    fun move() {}
+    fun move(userBridge : MutableList<String>, bridge : List<String>) : MutableList<String> {
+        var bridgeMatch = mutableListOf<String>()
+        for (i in 0 until userBridge.size){
+            if (userBridge == bridge) bridgeMatch.add("O")
+            if (userBridge != bridge) bridgeMatch.add("X")
+        }
+        return bridgeMatch
+    }
 
     fun retry() {}
 }
